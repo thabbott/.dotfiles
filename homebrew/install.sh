6f69d4f3f8119370d0b803f715e69784fe341ed0
Tristan Abbott <tristanhabbott@gmail.com>
2023-12-30 22:32:34 -0500

    setting up skhd

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -5,11 +5,15 @@ brew install stow
 brew install fd 
 brew install fzf 
 
-# Tiling window manager
+# Tiling window manager and hotkey daemon
 brew install yabai 
+brew install skhd
 
 # LSPs
 brew install lua-language-server
 
 # better grep used by telescope
 brew install ripgrep
+
+# json processor used by skhd setup
+brew install jq
