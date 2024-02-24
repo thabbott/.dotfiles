c408e6bc3548dad2273e35fcecce2969730f53b8
Tristan Abbott <tristanhabbott@gmail.com>
2024-02-23 20:53:11 -0500

    update

--- nvim/.config/nvim/lua/plugins/colors.lua
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -5,6 +5,8 @@ return {
 		priority = 1000,
 		config = function()
 			vim.cmd('colorscheme github_dark_high_contrast')
+         vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+         vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 		end,
 	}
 }
