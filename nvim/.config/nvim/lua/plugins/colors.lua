22c661261585f2596df7a51840d9e110fd4e911f
Tristan Abbott <tristanhabbott@gmail.com>
2024-02-24 00:52:30 -0500

    cleaning up

--- nvim/.config/nvim/lua/plugins/colors.lua
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -5,6 +5,7 @@ return {
 		priority = 1000,
 		config = function()
 			vim.cmd('colorscheme github_dark_high_contrast')
-		end,
+         vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+		end
 	}
 }
