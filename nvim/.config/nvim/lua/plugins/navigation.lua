31b9733858d37d71adac96f3a07bd9cfc7b54675
Tristan Abbott <tristanhabbott@gmail.com>
2024-03-09 12:46:30 -0500

    single repo for all machines

--- nvim/.config/nvim/lua/plugins/navigation.lua
+++ nvim/.config/nvim/lua/plugins/navigation.lua
@@ -6,4 +6,18 @@ return {
          require("leap").create_default_mappings()
       end
    },
+
+   -- between files
+   {
+      'nvim-telescope/telescope.nvim',
+      cond = not vim.g.vscode,
+      tag = '0.1.5',
+      dependencies = { 'nvim-lua/plenary.nvim' },
+      config = function()
+         local builtin = require('telescope.builtin')
+         vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+         vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+         vim.keymap.set('n', '<leader>fs', builtin.grep_string, {})
+      end
+   }
 }
