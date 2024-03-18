f865ed1170a75a879d01b6a5c4b62dde4231e6a6
Tristan Abbott <tristanhabbott@gmail.com>
2024-03-18 15:57:03 -0400

    update for linux

--- os/macos/setup.sh
+++ os/macos/setup.sh
@@ -15,6 +15,7 @@ stow -t $HOME -d machine/$(hostname) firefox
 stow -t $HOME -d os/macos skhd
 stow -t $HOME -d os/macos yabai
 stow -t $HOME -d os/macos git
+stow -t $HOME conda
 stow -t $HOME nvim
 stow -t $HOME tmux
 stow -t $HOME zsh
